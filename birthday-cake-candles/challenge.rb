9d77864489fdeb714ce4dd5852b5459b7236fa0a
Artem Hushcha <artem.hushcha@gmail.com>
2024-12-27 18:57:00 +0100

    Added testcases to birthday cake candles challenge

--- birthday-cake-candles/challenge.rb
+++ birthday-cake-candles/challenge.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# This is a class with challenge's solution
 class Challenge
   attr_accessor :input
 
@@ -8,11 +9,15 @@ class Challenge
   end
 
   def call
+    return 0 if @input.empty?
+    return 1 if @input.size == 1
+
     max = @input.shift
     counter = 1
     @input.each do |item|
       next if item < max
       counter += 1 and next if item == max
+
       max = item
       counter = 1
     end
--- birthday-cake-candles/challenge_test.rb
+++ birthday-cake-candles/challenge_test.rb
@@ -3,11 +3,39 @@
 require File.join(File.dirname(__FILE__), 'challenge')
 require 'test/unit'
 
+# This is a class with test cases
 class ChallengeTest < Test::Unit::TestCase
-  def test_2
-    input = [1, 3, 2, 1, 3]
+  def test_single_element
+    input = [5]
+    should_be_result = 1
+    actual_result = Challenge.new(input).call
+    assert_equal(should_be_result, actual_result)
+  end
 
+  def test_two_elements
+    input = [1, 3, 2, 1, 3]
     should_be_result = 2
+    actual_result = Challenge.new(input).call
+    assert_equal(should_be_result, actual_result)
+  end
+
+  def test_all_elements_equal
+    input = [4, 4, 4, 4]
+    should_be_result = 4
+    actual_result = Challenge.new(input).call
+    assert_equal(should_be_result, actual_result)
+  end
+
+  def test_no_elements
+    input = []
+    should_be_result = 0
+    actual_result = Challenge.new(input).call
+    assert_equal(should_be_result, actual_result)
+  end
+
+  def test_large_input
+    input = [1, 2, 3, 4, 5, 5, 5, 5]
+    should_be_result = 4
     actual_result = Challenge.new(input).call
     assert_equal(should_be_result, actual_result)
   end
