e3acbff4f1d411a33f9062df9bb3b36455d5eb3f
Artem Hushcha <artem.hushcha@gmail.com>
2025-01-17 16:53:00 +0100

    Add edge cases tests

--- bracket-matcher/challenge_test.rb
+++ bracket-matcher/challenge_test.rb
@@ -19,13 +19,6 @@ class ChallengeTest < Test::Unit::TestCase
     assert_equal(should_be_result, actual_result)
   end
 
-  def test_happy_path
-    input = '(c(oder)) b(yte)'
-    should_be_result = 1
-    actual_result = Challenge.new(input).call
-    assert_equal(should_be_result, actual_result)
-  end
-
   def test_empty_input
     input = ''
     should_be_result = 1
@@ -35,6 +28,48 @@ class ChallengeTest < Test::Unit::TestCase
 
   def test_without_brackets
     input = 'coderbyte'
+    should_be_result = 1
+    actual_result = Challenge.new(input).call
+    assert_equal(should_be_result, actual_result)
+  end
+
+  def test_nested_brackets
+    input = '((a(b)c)d)'
+    should_be_result = 1
+    actual_result = Challenge.new(input).call
+    assert_equal(should_be_result, actual_result)
+  end
+
+  def test_only_open_brackets
+    input = '(((('
+    should_be_result = 0
+    actual_result = Challenge.new(input).call
+    assert_equal(should_be_result, actual_result)
+  end
+
+  def test_only_close_brackets
+    input = '))))'
+    should_be_result = 0
+    actual_result = Challenge.new(input).call
+    assert_equal(should_be_result, actual_result)
+  end
+
+  def test_balanced_with_characters
+    input = '(a(b)c)d(e(f)g)'
+    should_be_result = 1
+    actual_result = Challenge.new(input).call
+    assert_equal(should_be_result, actual_result)
+  end
+
+  def test_unbalanced_with_characters
+    input = '(a(b)c)d(e(f)g'
+    should_be_result = 0
+    actual_result = Challenge.new(input).call
+    assert_equal(should_be_result, actual_result)
+  end
+
+  def test_happy_path
+    input = '(c(oder)) b(yte)'
     should_be_result = 1
     actual_result = Challenge.new(input).call
     assert_equal(should_be_result, actual_result)
