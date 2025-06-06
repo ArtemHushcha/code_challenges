8c9a192a8c77b702ce4a68616f7966b720e41c3b
Artem Hushcha <artem.hushcha@gmail.com>
2025-06-06 11:26:00 +0100

    feat: change implementation to 2 times array iteration

--- bracket-matcher/challenge.rb
+++ bracket-matcher/challenge.rb
@@ -12,14 +12,6 @@ class Challenge
   end
 
   def call
-    open_brackets_count = 0
-    close_brackets_count = 0
-
-    @input.chars.each do |char|
-      open_brackets_count += 1 and next if char == OPEN_BRACKET_CHAR
-      close_brackets_count += 1 and next if char == CLOSE_BRACKET_CHAR
-    end
-
-    open_brackets_count == close_brackets_count ? 1 : 0
+    @input.count(OPEN_BRACKET_CHAR) == @input.count(CLOSE_BRACKET_CHAR) ? 1 : 0
   end
 end
