65a985977f0dc1360a84315a48fec2596d316caa
Artem Hushcha <artem.hushcha@gmail.com>
2025-06-12 11:26:00 +0100

    feat: change implementation to 2 times array iteration in bc challenge

--- birthday-cake-candles/challenge.rb
+++ birthday-cake-candles/challenge.rb
@@ -9,15 +9,6 @@ class Challenge
   end
 
   def call
-    return 0 if @input.empty?
-
-    @input.each_with_object(height: 0, counter: 0) do |height, acc|
-      if height > acc[:height]
-        acc[:height] = height
-        acc[:counter] = 1
-      elsif height == acc[:height]
-        acc[:counter] += 1
-      end
-    end[:counter]
+    @input.count @input.max
   end
 end
